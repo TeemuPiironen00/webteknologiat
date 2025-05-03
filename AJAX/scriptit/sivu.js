2900dbd7ac67b0af403ae439fd1a67ac7be89781
Teemu Piironen <Teemu.Piironen@student.lab.fi>
2025-05-03 19:20:25 +0300

    i have added accordion to AJAX page

--- AJAX/scriptit/sivu.js
+++ AJAX/scriptit/sivu.js
@@ -10,6 +10,24 @@ function getNumber() {
         .then(function (result) {
             document.getElementById("gif").style.display = "none";
             document.getElementById("result").innerHTML += "<br>" + "<i>" + result + "</i>";
+
+            let data = `
+<div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${count}" aria-expanded="true" aria-controls="collapse${count}">
+        Random Math Fact #${count}
+      </button>
+    </h2>
+    <div id="collapse${count}" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">
+        ${result}
+      </div>
+    </div>
+  </div>`;
+            document.getElementById("accordionFlushExample").innerHTML += data;
+
+
+
         })
         .catch(function (error) {
             document.getElementById("gif").style.display = "none";
@@ -23,3 +41,4 @@ function countClick() {
     count++;
     document.getElementById('clickCount').textContent = count;
 }
+
