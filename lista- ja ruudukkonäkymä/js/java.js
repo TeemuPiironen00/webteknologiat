3f28ba4f2f84377fd3383791bd16020c7790ef50
Teemu <teemudanielpiironen@gmail.com>
2025-05-04 22:58:09 +0300

    Made grid / list website with gif background

---
+++ lista- ja ruudukkonäkymä/js/java.js
@@ -0,0 +1,14 @@
+function changeToGrid(){
+
+    document.getElementById('image-container').classList.remove('list-container');
+    document.getElementById('image-container').classList.add('grid-container');
+}
+
+function changeToList() {
+
+    document.getElementById('image-container').classList.remove('grid-container');
+    document.getElementById('image-container').classList.add('list-container');
+
+}
+
+
